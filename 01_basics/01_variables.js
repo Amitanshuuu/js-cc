92a232619c5e77c27eda453d5a34d211a04f8309
Amitanshuuu <125752258+Amitanshuuu@users.noreply.github.com>
2024-08-03 20:42:20 +0000

    new changes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 14453
+let accountEmail = "hdfc@gmail.com"
+var password = "1234567"
+city = "Bhubaneswar"
+let statename;  // a var is made without any value (i.e. a container without water)
+
+console.table([accountId,accountEmail,statename,city])  //used to print multiple var together
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+ 
+// alert(3 + 3) //we are using nodejs, not browser
+
+console.log(3 + 3);  //always miantain code readability
+
+
+let name = "Amitanshu"
+let age = 18
+let isLoggedIn = true
+
+
+// number 2 to power 53
+// bigint : more than the general integer range
+// strings 
+// boolean
+// null : generally null is a datatype with stanalone value
+// undefined : the value is not defined yet and yet is to be defined
+// sybmbol : to find uniqueness in the components
+// object
+
+console.log(typeof name);  // returns the datatype of the var
+
+/* questions 
+
+1) diff bw undefined and null
+2) why the datatype of null is object
+
+*/
